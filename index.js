40ea139f37498414bd9ef59ad95edf441e4a5ca1
adrienoleksiak <adrien.oleksiak.66@gmail.com>
2022-09-28 15:27:47 +0200

    lab 2

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello Node.js!")
+// Import a module
+const http = require('http')
+const url = require('url')
+
+/*
+// Declare an http server
+http.createServer(function (req, res) {
+
+  // Write a response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Write a response content
+  res.end('Hello World\n');
+
+// Start the server
+}).listen(8080)*/
+
+// Define a string constant concatenating strings
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello there!</p>' +
+'    </body>' +
+'</html>'
+
+
+const serverHandle = function (req, res) {
+  // Retrieve and print the current path
+  const path = url.parse(req.url).pathname;
+  console.log(path);
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(path);
+  res.end();
+}
+
+const server = http.createServer(serverHandle);
+server.listen(8080)
