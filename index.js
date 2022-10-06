335afcf4795e622d94a63f59c49d857342d9e2c0
adrienoleksiak <adrien.oleksiak.66@gmail.com>
2022-10-06 15:48:47 +0200

    test express

--- index.js
+++ index.js
@@ -29,12 +29,35 @@ http.createServer(serverHandle).listen(port,  _ => {
     console.log(`Server is running at http://localhost:${port}`)
 })*/
 
+let db = {
+    articles: [
+      {
+        id: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        title: 'My article',
+        content: 'Content of the article.',
+        date: '04/10/2022',
+        author: 'Liz Gringer'
+      },
+      // ...
+    ],
+    comments: [
+      {
+        id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+        timestamp: 1664835049,
+        content: 'Content of the comment.',
+        articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        author: 'Bob McLaren'
+      },
+      // ...
+    ]
+  }
+
 
 const app = express()
 const port = 8080
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send(db.articles)
 })
 
 app.listen(port)
