b4b975dad84346b62713e099d8204a3e29520c10
Dvdbx <73856214+Dvdbx@users.noreply.github.com>
2022-10-22 13:37:06 +0200

    fix: tailwinds css plugins

---
+++ app/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    colors: {
+      primary:"#1fb6ff",
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+    //extend: {},
+  },
+  plugins: [
+    require('tailwindcss-font-inter'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
+}
