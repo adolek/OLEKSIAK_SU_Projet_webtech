c50a72e08a24e09c682ce4233b95800bf9173d08
adrienoleksiak <adrien.oleksiak.66@gmail.com>
2022-12-13 17:55:10 +0100

    feat: init form signin with or without github

---
+++ app/components/account.jsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from 'react'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+
+export default function Account({ session }) {
+  const supabase = useSupabaseClient()
+  const user = useUser()
+  const [loading, setLoading] = useState(true)
+  const [full_name, setFull_name] = useState(null)
+  const [password, setPassword] = useState(null)
+  //const [avatar_url, setAvatarUrl] = useState(null)
+
+  useEffect(() => {
+    getProfile()
+  }, [session])
+
+  async function getProfile() {
+    try {
+      setLoading(true)
+
+      let { data, error, status } = await supabase
+        .from('profiles')
+        .select(`full_name, password`)
+        .eq('id', user.id)
+        .single()
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setFull_name(data.full_name)
+        setPassword(data.password)
+        //setAvatarUrl(data.avatar_url)
+      }
+    } catch (error) {
+      alert('Error loading user data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  async function updateProfile({ full_name, password }) {
+    try {
+      setLoading(true)
+
+      const updates = {
+        id: user.id,
+        updated_at: new Date().toISOString(),
+        full_name,
+        password,
+      }
+
+      let { error } = await supabase.from('profiles').upsert(updates)
+      if (error) throw error
+      alert('Profile updated!')
+    } catch (error) {
+      alert('Error updating the data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="form-widget">
+      <div>
+        <label htmlFor="email">Email</label>
+        <input 
+        id="email" 
+        type="text" 
+        value={session.user.email} disabled 
+        />
+      </div>
+      <div>
+        <label htmlFor="full_name">Full name</label>
+        <input
+          id="full_name"
+          type="text"
+          value={full_name || ''}
+          onChange={(e) => setFull_name(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={session.user.password} 
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <button
+          className="button primary block"
+          onClick={() => updateProfile({ full_name, password })}
+          disabled={loading}
+        >
+          {loading ? 'Loading ...' : 'Update'}
+        </button>
+      </div>
+
+      <div>
+        <button className="button block" onClick={() => supabase.auth.signOut()}>
+          Sign Out
+        </button>
+      </div>
+    </div>
+  )
+}
--- app/pages/login.js
+++ app/pages/login.js
@@ -1,42 +1,31 @@
 import React from "react";
 import supabase from "../supabaseClient";
-import { Auth } from "@supabase/auth-ui-react";
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Account from '../components/Account'
 import { useState } from "react";
 
 const Login = () => {
-  const customTheme = {
-    default: {
-      colors: {
-        brand: "hsl(153 60.0% 53.0%)",
-        brandAccent: "hsl(154 54.8% 45.1%)",
-        brandButtonText: "white",
-      },
-      dark: {
-        colors: {
-          brandButtonText: "white",
-          defaultButtonBackground: "#2e2e2e",
-          defaultButtonBackgroundHover: "#3e3e3e",
-        },
-      },
-      evenDarker: {
-        colors: {
-          brandButtonText: "white",
-          defaultButtonBackground: "#1e1e1e",
-          defaultButtonBackgroundHover: "#2e2e2e",
-        },
-      },
-    },
-  };
+  const session = useSession()
+  const supabase = useSupabaseClient()
 
   return (
     <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-      <Auth
-        supabaseClient={supabase}
-        theme="default"
-        appearance={{ theme: customTheme }}
-      />
+       {!session ? (
+        <Auth 
+          providers={["github"]}
+          supabaseClient={supabase} 
+          appearance={{ theme: ThemeSupa }} 
+          theme="light"
+        />
+      ) : (
+        <Account session={session} />
+      )}
+      
     </main>
   );
 };
 
 export default Login;
+
+ 
