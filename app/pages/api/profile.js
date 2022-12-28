b406cf16bedf597a399fec7fb58cc90bdf5a07bb
Dvdbx <73856214+Dvdbx@users.noreply.github.com>
2022-12-28 15:20:49 +0100

    fix: build error

--- app/pages/api/profile.js
+++
@@ -1,9 +0,0 @@
-const userProfile = {
-  username: "David",
-  email: "david@adaltas.com",
-};
-
-export default function handler(req, res) {
-  userProfile.isLoggedIn = true;
-  res.status(200).json({ ...userProfile, isLoggedIn: true });
-}
